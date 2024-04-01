0ab521ec5c41e0e18e6d0b5ba9427b5c28874b85
Shin-sibainu <shincode0712@gmail.com>
2024-04-01 10:54:34 +0900

    mod

--- app/api/stripe-hooks/route.ts
+++ app/api/stripe-hooks/route.ts
@@ -1,25 +1,47 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 import initStripe from "stripe";
+import { cookies } from "next/headers";
+
+export const config = { api: { bodyParser: false } };
 
 export async function POST(req: NextRequest) {
-  // console.log("event received");
-  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
-  const signature = req.headers.get("stripe-signature");
-  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+  const supabase = createRouteHandlerClient<Database>({ cookies });
 
-  let event;
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+  const signature = req.headers.get("stripe-signature")!;
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET!;
+
+  // リクエストのバッファをそのまま取得
+  const reqBuffer = Buffer.from(await req.arrayBuffer());
 
   try {
-    event = stripe.webhooks.constructEvent(req, signature, signingSecret);
+    // バッファをそのままconstructEventに渡す
+    const event = stripe.webhooks.constructEvent(
+      reqBuffer,
+      signature,
+      signingSecret
+    );
+
+    switch (event.type) {
+      case "customer.subscription.created":
+        await supabase
+          .from("profile")
+          .update({
+            is_subscribed: true,
+            interval: event.data.object.items.data[0].plan.interval,
+          })
+          .eq("stripe_customer", event.data.object.customer);
+    }
+
+    console.log(event);
+
+    return NextResponse.json({ received: true });
   } catch (error: any) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json(
       { message: `Webhook Error: ${error.message}` },
       { status: 401 }
     );
   }
-
-  console.log({ event });
-
-  return NextResponse.json({ received: true });
 }
